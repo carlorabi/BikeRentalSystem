5ab56bb50d90da6bd4d193e48c251e145cecc3f4
Carlo <113867873+carlorabi@users.noreply.github.com>
2022-12-06 22:21:56 +0800

    Update testBike.py

--- testBike.py
+++ testBike.py
@@ -1,1 +1,10 @@
+import unittest
+from datetime import datetime, timedelta
+from bikeRental import BikeRental, Customer
 
+class BikeRentalTest(unittest.TestCase):
+    def test_Bike_Rental_diplays_correct_stock(self):
+        shop1 = BikeRental()
+        shop2 = BikeRental(10)
+        self.assertEqual(shop1.displaystock(), 0)
+        self.assertEqual(shop2.displaystock(), 10)
