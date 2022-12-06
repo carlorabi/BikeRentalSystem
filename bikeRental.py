df28980fe004a113e4d1560c5002ea63adb26e80
Angelica Marie Cachero <116714598+angelicacachero@users.noreply.github.com>
2022-12-06 22:07:52 +0800

    Update bikeRental.py

--- bikeRental.py
+++ bikeRental.py
@@ -31,3 +31,24 @@ class BikeRental:
             print("\nYou will be charged ₱50.00 for each hour per bike. We hope that you enjoy our service!")
             self.stock -= n
             return now 
+
+    def rentBikeOnDailyBasis(self, n):
+        """
+        Rents a bike on daily basis to a customer.
+        """
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+
+        elif n > self.stock:
+            print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
+            return None
+    
+        else:
+            now = datetime.datetime.now()                      
+            print("You have rented {} bike(s) on daily basis today at {} hours.".format(n, now.hour))
+            print("You will be charged $20 for each day per bike.")
+            print("We hope that you enjoy our service.")
+
+            self.stock -= n
+            return now        
